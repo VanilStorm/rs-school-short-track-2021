d1ea61ce66d938c968e974c85da15ab182d8d23b
Ivan <77737846+VanilStorm@users.noreply.github.com>
2021-04-11 18:37:55 +0300

    fix: task2

--- src/02-common-character-count.js
+++ src/02-common-character-count.js
@@ -10,25 +10,22 @@
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
 function getCommonCharacterCount(s1, s2) {
-  s1 = s1.split('')
-  s2 = s2.split('')
-
+  const str1 = s1.split('');
+  const str2 = s2.split('');
   let a = 0;
-
-  if (s1.length === 0 || s2.length === 0) return a;
-
-for (let i = 0; i < s1.length; i++) {
-  for (let k = 0; k < s2.length; k++) {
-    if (s1[i] === s2[k]) {
-     a++;
-     s1.splice(i, 1);
-     s2.splice(k, 1);
-     i--;
-     break;
+  if (str1.length === 0 || str2.length === 0) return a;
+  for (let i = 0; i < str1.length; i++) {
+    for (let k = 0; k < str2.length; k++) {
+      if (str1[i] === str2[k]) {
+        a++;
+        str1.splice(i, 1);
+        str2.splice(k, 1);
+        i--;
+        break;
+      }
     }
-   }
-  }          
- return a;
+  }
+  return a;
 }
 
 module.exports = getCommonCharacterCount;
